f6b19296eeda21d784b9db5651c7e5b544316bf7
Tom Hennessy <tomlhennessy@outlook.com>
2024-07-09 13:04:17 +0100

    implemented and tested queue class

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,18 +5,40 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
-        // Add node to end of queue (linked list)
+        const newNode = new SinglyLinkedNode(val);
 
-        // Write your hypothesis on the time complexity of this method here
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this.length;
+        // time complexity: O(1) - adding a node to the tail is a constant time operation
     }
 
     dequeue() {
-        // Remove node from front of queue (linked list)
-        
-        // Write your hypothesis on the time complexity of this method here
+        if (this.length === 0) return null;
+
+        const removedHead = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+
+        this.length--;
+        return removedHead.value;
+        // time complexity: O(1) - removing the head is a constant time operation
     }
 
 }
